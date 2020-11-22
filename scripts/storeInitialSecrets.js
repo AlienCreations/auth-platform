83ab924879499f531029ca70b54a15b454b9f107
Sean Cannon <alienwebguy@gmail.com>
2020-11-22 12:10:58 -0800

    fix storeInitialSecrets syntax (#1)

--- scripts/storeInitialSecrets.js
+++ scripts/storeInitialSecrets.js
@@ -1,40 +1,15 @@
 'use strict';
 
-const R = require('ramda');
-
 const Secret = require('../server/core/services/secret/Secret')(process.env.SECRET_STRATEGY);
 
-const flattenAndObjectify = R.compose(
-  R.mapObjIndexed((v, k) => R.reject(R.equals(k), v)),
-  R.map(R.uniq),
-  R.map(R.flatten),
-  R.groupBy(R.head)
-);
-
-const makeWhitelistDict = R.compose(
-  JSON.stringify,
-  flattenAndObjectify,
-  R.apply(R.xprod),
-  v => [v, v]
-);
-
-const makePublicKeysDict = key => R.compose(
-  JSON.stringify,
-  R.map(R.always([key])),
-  R.invert
-);
-
 const services   = JSON.parse(process.env.SERVICES);
-const whitelist  = makeWhitelistDict(services);
-const publicKeys = makePublicKeysDict(process.env.SHARED_PUBLIC_KEY)(services);
-const privateKey = process.env.PRIVATE_KEY;
 
 const storePrivateKeys = () => Promise.all(services.map(
-  service => Secret.setItem(`secret/data/services/${service}/privateKey`, privateKey)
+  service => Secret.setItem(`secret/data/services/${service}/privateKey`, process.env.PRIVATE_KEY)
 ));
 
-const storePublicKeys = () => Secret.setItem('secret/data/services/publicKeys', publicKeys);
-const storeWhitelist  = () => Secret.setItem('secret/data/services/whitelist',  whitelist);
+const storePublicKeys = () => Secret.setItem('secret/data/services/publicKeys', process.env.PUBLIC_KEYS);
+const storeWhitelist  = () => Secret.setItem('secret/data/services/whitelist',  process.env.WHITELIST);
 
 module.exports = () => Promise.all([
   storePrivateKeys(),
