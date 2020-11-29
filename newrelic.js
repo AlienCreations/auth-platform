fb00a3df12fefefa337aaf4ee30cdae99a2427ed
seancannon <alienwebguy@gmail.com>
2020-11-28 16:16:35 -0800

    add newrelic.js/

---
+++ newrelic.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config/default.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  app_name    : [process.env.THIS_SERVICE_NAME],
+  license_key : process.env.NEW_RELIC_LICENSE_KEY
+};
