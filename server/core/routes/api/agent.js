ff7328745253a6a02b8f0e1cf94da5ef62a50f6b
seancannon <alienwebguy@gmail.com>
2023-06-19 12:35:49 -0700

    add uuid support to agent routes

--- server/core/routes/api/agent.js
+++ server/core/routes/api/agent.js
@@ -5,10 +5,11 @@ const express  = require('express'),
       router   = express.Router(),
       apiUtils = require('../../utils/api');
 
-const createAgent   = require('../../controllers/api/agent/createAgent'),
-      updateAgent   = require('../../controllers/api/agent/updateAgent'),
-      deleteAgent   = require('../../controllers/api/agent/deleteAgent'),
-      getAgentByKey = require('../../controllers/api/agent/getAgentByKey');
+const createAgent    = require('../../controllers/api/agent/createAgent'),
+      updateAgent    = require('../../controllers/api/agent/updateAgent'),
+      deleteAgent    = require('../../controllers/api/agent/deleteAgent'),
+      getAgentByKey  = require('../../controllers/api/agent/getAgentByKey'),
+      getAgentByUuid = require('../../controllers/api/agent/getAgentByUuid');
 
 const { ensureAuthorized } = require('@aliencreations/node-authenticator')(config.auth.strategy);
 
@@ -24,23 +25,23 @@ router.post('/', ensureAuthorized, (req, res, next) => {
   );
 });
 
-// https://platform.aliencreations.com/api/v1/agent/key/cixs563xk0000m4mrd68m0lrc
-router.put('/key/:key', ensureAuthorized, (req, res, next) => {
-  const key = req.params.key;
+// https://platform.aliencreations.com/api/v1/agent/uuid/a9445fa7-52dd-43eb-a377-327e63475a7f
+router.put('/uuid/:uuid', ensureAuthorized, (req, res, next) => {
+  const { uuid } = req.params;
 
   apiUtils.respondWithErrorHandling(
     req,
     res,
     next,
-    req.logger.child({ key }),
+    req.logger.child({ uuid }),
     'updateAgent',
-    () => updateAgent(req.body, key)
+    () => updateAgent(req.body, uuid)
   );
 });
 
 // https://platform.aliencreations.com/api/v1/agent/key/cixs563xk0000m4mrd68m0lrc
 router.get('/key/:key', ensureAuthorized, (req, res, next) => {
-  const key = req.params.key;
+  const { key } = req.params;
 
   apiUtils.respondWithErrorHandling(
     req,
@@ -52,17 +53,31 @@ router.get('/key/:key', ensureAuthorized, (req, res, next) => {
   );
 });
 
-// https://platform.aliencreations.com/api/v1/agent/key/cixs563xk0000m4mrd68m0lrc
-router.delete('/key/:key', ensureAuthorized, (req, res, next) => {
-  const key = req.params.key;
+// https://platform.aliencreations.com/api/v1/agent/uuid/a9445fa7-52dd-43eb-a377-327e63475a7f
+router.get('/uuid/:uuid', ensureAuthorized, (req, res, next) => {
+  const { uuid } = req.params;
 
   apiUtils.respondWithErrorHandling(
     req,
     res,
     next,
-    req.logger.child({ key }),
+    req.logger.child({ uuid }),
+    'getAgentByUuid',
+    () => getAgentByUuid(uuid)
+  );
+});
+
+// https://platform.aliencreations.com/api/v1/agent/uuid/a9445fa7-52dd-43eb-a377-327e63475a7f
+router.delete('/uuid/:uuid', ensureAuthorized, (req, res, next) => {
+  const { uuid } = req.params;
+
+  apiUtils.respondWithErrorHandling(
+    req,
+    res,
+    next,
+    req.logger.child({ uuid }),
     'deleteAgent',
-    () => deleteAgent(key)
+    () => deleteAgent(uuid)
   );
 });
 
